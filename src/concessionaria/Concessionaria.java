29e533ab4edd1a3dc943c3600bf68ef11a5c1a56
Guilherme <guilhermecd2005@gmail.com>
2025-10-19 23:02:44 -0300

    Concessionaria.java

--- src/concessionaria/Concessionaria.java
+++ src/concessionaria/Concessionaria.java
@@ -15,18 +15,31 @@ public class Concessionaria {
         while (opt != 0)
         System.out.println("|-----Gerenciamento Concessionaria-----|");
         System.out.println("| 1 - Gerenciamento de Clientes:       |");
-        System.out.println("| 2");
-        System.out.println("| 3");
-        System.out.println("| 4");
-        System.out.println("| 5");
-        System.out.println("| 0");
+        System.out.println("| 2 - Gerenciamento de Funcionarios:   |");
+        System.out.println("| 3 - Gerenciamento de Veiculos:       |");
+        System.out.println("| 4 - Gerenciamento de Venda:          |");
+        System.out.println("| 5 - Gerenciamento de Relatorio:      |");
+        System.out.println("| 0 - Sair do Programa                 |");
         leitura.nextLine();
-        switch(opt){
-            case 1:{
-                
+        switch(opt) {
+            case 1: {
+                //Menu_Cliente();
             }
-                
-                    
+            case 2: {
+                //Menu_Funcionario();
+            }
+            case 3: {
+                //Menu_Veiculos();
+            }
+            case 4: {
+                //Menu_Venda();
+            }
+            case 5: {
+                //Menu_Relatorio();
+            }
+            case 0: {
+                System.out.println("Finalizando o programa!");
+            }
         }
     }
 }
