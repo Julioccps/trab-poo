2218070fbb86a0a0f025027957c5f646f2527bec
Guilherme <guilhermecd2005@gmail.com>
2025-10-21 22:42:21 -0300

    Inter.Java

--- src/concessionaria/Inter.java
+++ src/concessionaria/Inter.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Inter {
-    public Inter(){
+    public Inter() {
         super();
     }
-    public void menuCliente(Scanner leitura, ArrayList<Cliente> clientes){
+    public void menuCliente(Scanner leitura, ArrayList<Cliente> clientes) {
         int opt;
         opt = -1;
-        while (opt != 0){
+        while (opt != 0) {
             System.out.println("|----- Menu Clientes ------------|");
             System.out.println("|--- 1 Cadastrar cliente --------|");
             System.out.println("|--- 2 Consultar cliente --------|");
@@ -19,8 +19,8 @@ public class Inter {
             System.out.println("|--- 0 Voltar ao menu principal -|");
             opt = leitura.nextInt();
             leitura.nextLine();
-            switch (opt){
-                case 1:{
+            switch (opt) {
+                case 1: {
                     System.out.print("Nome do cliente: ");
                     String nome = leitura.nextLine();
                     System.out.print("Telefone do cliente: ");
@@ -37,7 +37,7 @@ public class Inter {
                     System.out.println("Cliente adicionado com sucesso");
                     opt = -1;
                 }
-                case 2:{
+                case 2: {
                     System.out.println("|---------- Clientes ------------|");
                     for (int i = 0; i < clientes.size(); i++){
                         System.out.println("  " + (i) + clientes.get(i).getNome());
@@ -47,11 +47,109 @@ public class Inter {
                     clientes.get(opt).consultar();
                     opt = -1;
                 }
-                case 3:{
+                case 3: {
+                    
+                }
+                case 4: {
+                    
+                }
+                case 0: {
                     
                 }
             }
-            
+        }
+    }
+    public void menuFuncionario(Scanner leitura, ArrayList<Funcionario> funcionarios) {
+        int opt;
+        opt = -1;
+        while (opt != 0){
+            System.out.println("|----- Menu Funcionarios ------------|");
+            System.out.println("|--- 1 Cadastrar Funcionario --------|");
+            System.out.println("|--- 2 Consultar Funcionario --------|");
+            System.out.println("|--- 3 Alterar Funcionario ----------|");
+            System.out.println("|--- 4 Remover Funcionario ----------|");
+            System.out.println("|--- 0 Voltar ao menu principal -|");
+            opt = leitura.nextInt();
+            leitura.nextLine();
+            switch (opt) {
+                case 1: {
+                    
+                }
+                case 2: {
+                    
+                }
+                case 3: {
+                    
+                }
+                case 4: {
+                    
+                }
+                case 0: {
+                    
+                }
+            }    
+        }
+    }
+    public void menuVeiculo(Scanner leitura, ArrayList<Veiculo> veiculos) {
+        int opt;
+        opt = -1;
+        while (opt != 0){
+            System.out.println("|----- Menu Veiculos ------------|");
+            System.out.println("|--- 1 Cadastrar Veiculo --------|");
+            System.out.println("|--- 2 Consultar Veiculo --------|");
+            System.out.println("|--- 3 Alterar Veiculo ----------|");
+            System.out.println("|--- 4 Remover Veiculo ----------|");
+            System.out.println("|--- 0 Voltar ao menu principal -|");
+            opt = leitura.nextInt();
+            leitura.nextLine();
+            switch (opt) {
+                case 1: {
+                    
+                }
+                case 2: {
+                    
+                }
+                case 3: {
+                    
+                }
+                case 4: {
+                    
+                }
+                case 0: {
+                    
+                }
+            }
+        }
+    }
+    public void menuVenda(Scanner leitura, ArrayList<Venda> vendas) {
+        int opt;
+        opt = -1;
+        while (opt != 0) {
+            System.out.println("|----- Menu Vendas ------------|");
+            System.out.println("|--- 1 Cadastrar Venda --------|");
+            System.out.println("|--- 2 Consultar Venda --------|");
+            System.out.println("|--- 3 Alterar Venda ----------|");
+            System.out.println("|--- 4 Remover Venda ----------|");
+            System.out.println("|--- 0 Voltar ao menu principal -|");
+            opt = leitura.nextInt();
+            leitura.nextLine();
+            switch (opt) {
+                case 1: {
+                    
+                }
+                case 2: {
+                    
+                }
+                case 3: {
+                    
+                }
+                case 4: {
+                    
+                }
+                case 0: {
+                    
+                }
+            }
         }
     }
 }
